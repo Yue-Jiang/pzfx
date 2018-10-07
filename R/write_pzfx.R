d5833890506b9b19e26ed656d03bf6c0567c95ea
yuejiang <rivehill@gmail.com>
2018-10-07 15:19:28 -0700

    add ability to write "XY" tables - I think this is the main type of tables in Prism.

--- R/write_pzfx.R
+++ R/write_pzfx.R
@@ -7,7 +7,10 @@
 #' @param path Path to the output '.pzfx' file.
 #' @param row_names Logical. If row names of the input table be preserved and become row titles in
 #'   the output '.pzfx' file. If the length is greater than 1, it must match the length of list of
-#'   input tables.
+#'   input tables. Default: TRUE.
+#' @param x_col 1-based column index or name of the column to be used as the 'X' column. If the
+#'   length is greater than 1, it must match the length of list of input tables. All other columns
+#'   in the input tables will be treated as "Y" columns in the output '.pzfx' file. Default: NA
 #'
 #' @return write_pzfx returns the input x invisibly.
 #'
@@ -17,7 +20,7 @@
 #' pzfx_file <- system.file("extdata/exponential_decay.pzfx", package = "pzfx", mustWork = TRUE)
 #' df <- read_pzfx(pzfx_file, table = 1, strike_action = "exclude")
 #' write_pzfx(df, path = tempfile(fileext = ".pzfx"), row_names = TRUE)
-write_pzfx <- function(x, path, row_names=TRUE) {
+write_pzfx <- function(x, path, row_names=TRUE, x_col=NA) {
   # figure out if x is a single table or multiple of them
   if (inherits(x, c("data.frame", "matrix"))) {
     x_lst <- list("Data 1"=x)
@@ -39,10 +42,41 @@ write_pzfx <- function(x, path, row_names=TRUE) {
   if (length(row_names) != length(x_lst)) {
     stop("Argument 'row_names' can only be of length 1 or the length of 'x'")
   }
+  # convert other kinds of x_col specifications to a vector of integers
+  if (length(x_col) == 1) x_col <- rep(x_col, length(x_lst))
+  if (length(x_col) != length(x_lst)) {
+    stop("Argument 'x_col' can only be of length 1 or the length of 'x'")
+  }
+  x_col[is.na(x_col)] <- 0
+  if (is.numeric(x_col)) {
+    x_col <- as.integer(x_col)
+    if (any(x_col > sapply(x_lst, ncol))) {
+      vio <- which(x_col > sapply(x_lst, ncol))
+      stop(sprintf("Not enough columns for table %s", paste(names(x_lst)[vio], collapse=", ")))
+    }
+  }
+  if (is.character(x_col)) {
+    for (i in seq_len(length(x_col))) {
+      idx <- which(colnames(x_lst[[i]]) == x_col[i])
+      if (length(idx) == 0) {
+        warning(sprintf(
+          "Column %s is not in table %s, not used as 'X' column",
+          x_col[i], names(x_lst[[i]])))
+        x_col[i] <- 0
+      } else if (length(idx) > 1) {
+        warning(sprintf(
+          "Column %s has multiple occurance in table %s, only the first one used as 'X' column",
+          x_col[i], names(x_lst[[i]])))
+        x_col[i] <- idx[1]
+      } else {
+        x_col[i] <- idx
+      }
+    }
+  }
 
   lst <- base_lst()
   lst$GraphPadPrismFile$TableSequence <- table_seq_lst(x_lst)
-  lst$GraphPadPrismFile <- append(lst$GraphPadPrismFile, table_lst(x_lst, row_names))
+  lst$GraphPadPrismFile <- append(lst$GraphPadPrismFile, table_lst(x_lst, row_names, x_col))
   attr(lst$GraphPadPrismFile, "PrismXMLVersion") <- "5.00"
   xml <- xml2::as_xml_document(lst)
   xml2::write_xml(xml, path)
@@ -107,9 +141,15 @@ table_seq_lst <- function(x_lst) {
 # "Table" elements of the list for a pzfx xml
 # As many tables as you have
 # Currently only supports pzfx's "Column" type of tables
-table_lst <- function(x_lst, row_names) {
+table_lst <- function(x_lst, row_names, x_col) {
   if (length(x_lst) != length(row_names)) {
     stop("Argument 'row_names' can only be of the same length as 'x_lst'")
+  }
+  if (length(x_lst) != length(x_col)) {
+    stop("Argument 'x_col' can only be of the same length as 'x_lst'")
+  }
+  if (!is.integer(x_col)) {
+    stop("Argument 'x_col' can only be of type 'integer'")
   }
   subcol_helper <- function(v) {
     v <- as.vector(v)
@@ -129,6 +169,7 @@ table_lst <- function(x_lst, row_names) {
       )
     })
     names(cols) <- rep("YColumn", ncol(this_df))
+    names(cols)[x_col[i]] <- "XColumn"
     one_table_lst <- list("Title"=list(names(x_lst)[i]))
     if (row_names[i]) {
       one_table_lst[["RowTitlesColumn"]] <- structure(
@@ -145,6 +186,12 @@ table_lst <- function(x_lst, row_names) {
       TableType="OneWay",
       EVFormat="AsteriskAfterNumber"
     )
+    if (x_col[i] > 0) {
+      attr(one_table, "XFormat") <- "numbers"
+      attr(one_table, "YFormat") <- "replicates"
+      attr(one_table, "Replicates") <- "1"
+      attr(one_table, "TableType") <- "XY"
+    }
     return(one_table)
   })
   names(ret) <- rep("Table", length(x_lst))
